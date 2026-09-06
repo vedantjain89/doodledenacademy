0c68df74b009c9fac3054dcf483a51b0b5b46881
vedantjain89 <vedantjck@gmail.com>
2026-09-06 20:04:54 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/doodledenacademy/',
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: false
-  }
 })
